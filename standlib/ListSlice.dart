ebce98647627d791158c5f1beba4db5a8f111215
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2019-03-01 20:58:44 +0100

    modify ListSlice for Dart2

--- standlib/ListSlice.dart
+++ standlib/ListSlice.dart
@@ -1,61 +1,36 @@
 // ListSlice is the emulator for go slices in dart.
 class ListSlice {
-List source;
-int start = 0;
-int end = 0;
-int length = 0;
+  List source;
 
-    ListSlice([int this.length=-1, List this.source, this.start = 0, this.end = 0]) {
-        if (this.source == null) {
-          this.source = new List();
-        }
+  ListSlice(this.source);
 
+  ListSlice slice(int subStart, int subEnd) {
+    ListSlice sliced;
+    sliced = ListSlice(source.sublist(subStart, subEnd));
+    return sliced;
+  }
 
-        if (start == end && source.length > 0) {
-          end = source.length;
-        }
-        if (length == -1) {
-            length = end - start;
-        }
+  dynamic elementAt(int index) {
+    return source.elementAt(index);
+  }
+
+  void setAt(int index, val) {
+    source[index] = val;
+  }
+
+  String toString() {
+    String result = "";
+    for (int i = 0; i < source.length; i++) {
+      result += source[i].toString();
     }
+    return result;
+  }
 
-    ListSlice slice(int subStart, int subEnd) {
-        return new ListSlice(source, start + subStart, start + subEnd);
-    }
+  void add(element) {
+    source.add(element);
+  }
 
-    dynamic elementAt(int index) {
-        int sourceIndex = start + index;
-        if (sourceIndex < start || sourceIndex >= end || sourceIndex < 0 || sourceIndex >= source.length) {
-          return null;
-        }
-        return source[sourceIndex];
-    }
-
-    setAt(int index, val) {
-        int sourceIndex = start + index;
-        if (sourceIndex < start || sourceIndex >= end || sourceIndex < 0 || sourceIndex >= source.length) {
-          return; // throw?
-        }
-        source[sourceIndex] = val;
-    }
-
-    String toString() {
-        String result = "";
-        for (int i = start; i < end; i++) {
-          result += source[i].toString();
-        }
-        return result;
-    }
-
-    void add(element) {
-        source.add(element);
-        end++;
-        length++;
-    }
-
-    void copy(src) {
-        this.source = new List.from(src);
-        this.start = 0;
-        this.end = this.source.length;
-    }
+  void copy(src) {
+    this.source = List.from(src);
+  }
 }
